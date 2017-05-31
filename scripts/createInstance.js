0ef6b4b57d2da1f140cf6676d2508f184831e015
Roderick Hsiao <roderick.hsiao@gotinder.com>
2017-05-31 16:44:52 -0700

    change to west

--- scripts/createInstance.js
+++ scripts/createInstance.js
@@ -2,7 +2,7 @@ import AWS from 'aws-sdk';
 
 // TODO, Load config
 const eb = new AWS.ElasticBeanstalk({
-  region: 'us-east-1'
+  region: 'us-west-1'
 });
 const BRANCH_NAME = process.env.CIRCLE_BRANCH || 'demo';
 
