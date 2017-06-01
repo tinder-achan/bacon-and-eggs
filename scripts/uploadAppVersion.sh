4771e0591c6b18f33c0f08d8cf4490f44f642d08
Erik Hellenbrand <erik.hellenbrand@gotinder.com>
2017-06-01 01:43:25 -0700

    use aws cli

--- scripts/uploadAppVersion.sh
+++ scripts/uploadAppVersion.sh
@@ -1,7 +1,7 @@
 S3_REGION=us-west-1
 S3_BUCKET=elasticbeanstalk-us-west-1-648980024845/sandboxes
 ZIP_FILENAME=$CIRCLE_BRANCH-$CIRCLE_SHA1-$CIRCLE_BUILD_NUM.zip
-
+ENV_TEMPLATE=sandboxes
 
 if [[ -z $S3_BUCKET ]]; then
 
@@ -13,9 +13,17 @@ END
   exit 1
 fi
 
-
 git archive --format=zip HEAD > $ZIP_FILENAME
+
 aws configure set default.region $S3_REGION
 
 aws s3 cp $ZIP_FILENAME s3://$S3_BUCKET/$ZIP_FILENAME
 
+echo "Uploaded source to S3"
+
+aws elasticbeanstalk create-environment --application-name tester --environment-name $CIRCLE_BRANCH --template-name $ENV_TEMPLATE
+echo "Created environment"
+aws elasticbeanstalk create-application-version --source-bundle S3Bucket=$S3_BUCKET,S3Key=$ZIP_FILENAME
+echo "Created application version"
+aws elasticbeanstalk update-environment --application-name tester --environment-name $CIRCLE_BRANCH --template-name $ENV_TEMPLATE
+echo "Updated environment"
