0423b1784403bdaabc4e0a8fe13598ce93f4393d
Erik Hellenbrand <erik.hellenbrand@gotinder.com>
2017-06-01 13:57:07 -0700

    no more sad

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', function(req, res){
-  res.send('hello 🥓 & 🍳 & sad');
+  res.send('hello 🥓 & 🍳');
 });
 
 app.listen(process.env.PORT || 3000);
